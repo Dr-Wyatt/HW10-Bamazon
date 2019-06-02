33acfa10704ecc2d339a2d226c8c91ea8bf070da
Daniel <daniel.ryuichi.wyatt@gmail.com>
2019-06-02 18:07:22 -0500

    Added add to inventory function

--- Bamazon_JS_Files/bamazonManager.js
+++ Bamazon_JS_Files/bamazonManager.js
@@ -26,11 +26,11 @@ function optionsMenu() {
     ]).then(answers => {
         if (answers.Options == "View Products for Sale") {
             viewProducts();
-        } else if (answers.Options == "View Low Inventory"){
+        } else if (answers.Options == "View Low Inventory") {
             viewLowInventory();
         } else if (answers.Options == "Add to Inventory") {
-            console.log(answers.Options);
-        } else if (answers.Options == "Add New Product"){
+            addInventory();
+        } else if (answers.Options == "Add New Product") {
             console.log(answers.Options);
         } else {
             connection.end();
@@ -40,7 +40,7 @@ function optionsMenu() {
 }
 
 
-function viewProducts(){
+function viewProducts() {
     connection.query("SELECT item_id, product_name, price, stock_quantity FROM products", function (err, res) {
         if (err) throw err;
         console.table(res);
@@ -48,10 +48,36 @@ function viewProducts(){
     });
 }
 
-function viewLowInventory(){
+function viewLowInventory() {
     connection.query("SELECT * FROM products WHERE stock_quantity < 5", function (err, res) {
         if (err) throw err;
         console.table(res);
         optionsMenu();
     });
 }
+
+function addInventory() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        inquirer.prompt([
+            {
+                message: "Which item would you like to add inventory?",
+                name: "add_Inventory",
+                type: "list",
+                choices: res.map(item => item.item_id)
+            },
+            {
+                message: "How much would you like to add?",
+                name: "inventory_Amount"
+            }
+        ]).then(answers => {
+            let inventoryItem = res.find(item => item.item_id == answers.add_Inventory);
+            let newStock = inventoryItem.stock_quantity + parseInt(answers.inventory_Amount);
+            connection.query("UPDATE products SET ? WHERE ?", [{ stock_quantity: newStock }, { item_id: inventoryItem.item_id }], (err, res) => {
+                console.log("Successfully Added!");
+                console.log(`There is now ${newStock} of that product`);
+                optionsMenu();
+            });
+        });
+    });
+} 
